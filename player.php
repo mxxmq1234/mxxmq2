af6e0937e15d33e78a2a0d620a92a013861b964b
mxxmq123 <32786337+mxxmq123@users.noreply.github.com>
2024-05-19 11:04:14 +0300

    Update player.php

--- player.php
+++ player.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="referrer" content="no-referrer" />
-    <title>İngSport TV</title>
+    <title>NETV GOLD</title>
     <meta name="keywords" content="oplayer,online player" />
     <style>
       body,
